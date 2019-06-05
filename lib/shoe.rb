1791f4b6e4a33675334e7487be6dcc1f517d394e
Danielle Mizrachi <dmiz@brandeis.edu>
2019-06-05 13:22:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,6 @@
 class Book
-  attr_accessor :shoe, :brand, :genre
+  attr_accessor :shoe, :brand, :color
  
-  def initialize(title)
-    @title = title
-  end
  
-  def title
-    @title
-  end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
  
 end
